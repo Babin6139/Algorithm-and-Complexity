fcbeb493d2ea55edda52c96ecc8e4abf010f0da9
Babin6139 <khatribabin39@gmail.com>
2022-03-29 18:36:25 +0545

    Lab2 completed

--- lab2/src/lib.rs
+++ lab2/src/lib.rs
@@ -12,30 +12,39 @@ mod tests {
 
         insertion_sort(&mut data);
         insertion_sort(&mut data1);
+        insertion_sort(&mut data2);
         assert_eq!(data1, [1, 2, 3, 4, 5]);
         assert_eq!(data, [2, 3, 5, 6, 9]);
+        assert_eq!(data2, [1.2, 1.5, 2.1, 2.3, 6.3]);
     }
 
     #[test]
     fn merge_test() {
         let mut data: Vec<i32> = vec![5, 6, 3, 9, 2];
         let mut data1: Vec<i32> = vec![1, 2, 3, 4, 5];
+        let mut data2: Vec<f32> = vec![1.5, 2.3, 0.5];
         merge(&mut data, 0, 1, 2);
         merge(&mut data1, 0, 1, 2);
+        merge(&mut data2, 0, 1, 2);
         assert_eq!(data, [3, 5, 6, 9, 2]);
         assert_eq!(data1, [1, 2, 3, 4, 5]);
+        assert_eq!(data2, [0.5, 1.5, 2.3]);
     }
 
     #[test]
     fn merge_sort_test() {
         let mut data: Vec<i32> = vec![5, 6, 3, 9, 2];
         let mut data1: Vec<i32> = vec![1, 2, 3, 4, 5];
-        let length = data.len();
-        let length1 = data1.len();
-        merge_sort(&mut data, 0, length - 1);
-        merge_sort(&mut data1, 0, length1 - 1);
+        let mut data2: Vec<f32> = vec![1.5, 1.3, 0.5, 2.5, 3.3];
+        let length = data.len() - 1;
+        let length1 = data1.len() - 1;
+        let length2 = data2.len() - 1;
+        merge_sort(&mut data, 0, length);
+        merge_sort(&mut data1, 0, length1);
+        merge_sort(&mut data2, 0, length2);
         assert_eq!(data, [2, 3, 5, 6, 9]);
         assert_eq!(data1, [1, 2, 3, 4, 5]);
+        assert_eq!(data2, [0.5, 1.3, 1.5, 2.5, 3.3]);
     }
 }
 
@@ -51,7 +60,19 @@ pub fn insertion_sort<T: PartialOrd + Copy>(unsorted_data: &mut [T]) {
     }
 }
 
-pub fn merge_sort(unsorted_data: &mut [i32], p: usize, r: usize) {
+// pub fn reverse_insertion_sort<T: PartialOrd + Copy>(unsorted_data: &mut [T]) {
+//     for j in 1..unsorted_data.len() {
+//         let data = unsorted_data[j];
+//         let mut i: i32 = (j - 1) as i32;
+//         while i >= 0 && unsorted_data[i as usize] < data {
+//             unsorted_data[(i + 1) as usize] = unsorted_data[i as usize];
+//             i = i - 1;
+//         }
+//         unsorted_data[(i + 1) as usize] = data;
+//     }
+// }
+
+pub fn merge_sort<T: PartialOrd + Copy>(unsorted_data: &mut [T], p: usize, r: usize) {
     if p < r {
         let q = (p + r) / 2;
         merge_sort(unsorted_data, p, q);
@@ -60,12 +81,11 @@ pub fn merge_sort(unsorted_data: &mut [i32], p: usize, r: usize) {
     }
 }
 
-pub fn merge(unsorted_data: &mut [i32], p: usize, q: usize, r: usize) -> () {
+pub fn merge<T: PartialOrd + Copy>(unsorted_data: &mut [T], p: usize, q: usize, r: usize) -> () {
     let L1 = unsorted_data[p..q + 1].to_vec();
     let R1 = unsorted_data[q + 1..r + 1].to_vec();
     let (mut i, mut j) = (0, 0);
     for k in p..r + 1 {
-        println!("{}", k);
         if i >= L1.len() {
             unsorted_data[k] = R1[j];
             j = j + 1;
--- lab2/src/main.rs
+++ lab2/src/main.rs
@@ -1,11 +1,85 @@
 use lab2::{insertion_sort, merge_sort};
+use plotlib::page::Page;
+use plotlib::repr::Plot;
+use plotlib::style::{LineJoin, LineStyle, PointMarker, PointStyle};
+use plotlib::view::ContinuousView;
 use std::time::Instant;
 
 fn main() {
-    let mut datas = vec![5, 6, 3, 2, 9];
-    let duration = Instant::now();
-    insertion_sort(&mut datas);
-    let mut datas = vec![5, 6, 3, 2, 9, 1];
-    let length = datas.len();
-    merge_sort(&mut datas, 0, length - 1);
+    let mut c: u64 = 0;
+    let mut insertionsort_times_best: Vec<(f64, f64)> = Vec::new();
+    let mut insertionsort_times_worst: Vec<(f64, f64)> = Vec::new();
+    let mut merge_sort_time: Vec<(f64, f64)> = Vec::new();
+    for _n in 0..20 {
+        c = c + 5000;
+        let mut vals: Vec<u64> = (0..c).collect();
+
+        let start = Instant::now();
+        insertion_sort(&mut vals);
+        let duration = start.elapsed().as_micros() as f64;
+        insertionsort_times_best.push((c as f64, duration));
+        let length = vals.len() - 1;
+        let _ = &mut vals.reverse();
+        let start = Instant::now();
+        insertion_sort(&mut vals);
+        let duration = start.elapsed().as_millis() as f64;
+        insertionsort_times_worst.push((c as f64, duration));
+        let start = Instant::now();
+        merge_sort(&mut vals, 0, length);
+        let duration = start.elapsed().as_millis() as f64;
+        merge_sort_time.push((c as f64, duration));
+    }
+    let insertion_plot_worst: Plot = Plot::new(insertionsort_times_worst)
+        .legend(String::from("Worst case"))
+        .point_style(
+            PointStyle::new()
+                .colour("black")
+                .marker(PointMarker::Circle),
+        )
+        .line_style(LineStyle::new().colour("red"));
+
+    let insertion_plot_best: Plot = Plot::new(insertionsort_times_best)
+        .legend(String::from("Best case"))
+        .point_style(
+            PointStyle::new()
+                .colour("black")
+                .marker(PointMarker::Circle),
+        )
+        .line_style(LineStyle::new().linejoin(LineJoin::Round).colour("green"));
+    let merge_plot: Plot = Plot::new(merge_sort_time)
+        .legend(String::from("Best case"))
+        .point_style(
+            PointStyle::new()
+                .colour("black")
+                .marker(PointMarker::Circle),
+        )
+        .line_style(LineStyle::new().colour("green").linejoin(LineJoin::Round));
+    let insertion_graph_best = ContinuousView::new()
+        .add(insertion_plot_best)
+        .x_range(0., 100000.)
+        .y_range(0., 10000.)
+        .x_label("Input-Size")
+        .y_label("Execution-Time (microsecond)");
+
+    let insertion_graph_worst = ContinuousView::new()
+        .add(insertion_plot_worst)
+        .x_range(0., 100000.)
+        .y_range(0., 60000.)
+        .x_label("Input-Size")
+        .y_label("Execution-Time (millisecond)");
+
+    let merge_graph = ContinuousView::new()
+        .add(merge_plot)
+        .x_range(0., 100000.)
+        .y_range(0., 300.)
+        .x_label("Input-Size")
+        .y_label("Execution-Time (millisecond)");
+
+    Page::single(&insertion_graph_best)
+        .save("insertion_best.svg")
+        .unwrap();
+    Page::single(&insertion_graph_worst)
+        .save("insertion_worst.svg")
+        .unwrap();
+    Page::single(&merge_graph).save("mergeSort.svg").unwrap();
 }
